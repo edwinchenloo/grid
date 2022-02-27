bbb1cb0d3c236a95d41b74d8b97817bc2cb6d1cb
edwinchenloo <edwinchenloo@gmail.com>
2022-02-26 22:31:10 -0600

    precompiled header

---
+++ GridPch.h
@@ -0,0 +1,21 @@
+#include <QApplication>
+#include <QFont>
+#include <QFontMetrics>
+#include <QGraphicsLineItem>
+#include <QGraphicsScene>
+#include <QGraphicsView>
+#include <QResizeEvent>
+#include <QScrollBar>
+#include <QTimer>
+#include <QtGui>
+#include <QtGui>
+
+#include <cassert>
+#include <ciso646>
+#include <cstddef>
+#include <cstdio>
+#include <cstdlib>
+#include <map>
+#include <string>
+#include <vector>
+
