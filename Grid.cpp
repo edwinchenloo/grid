9370b62ec5c02c3c7e8f6976a3cdcaf704f57a65
Edwin Chen <edwinchenloo@gmail.com>
2022-03-02 00:28:23 -0600

    crash when exiting

--- Grid.cpp
+++ Grid.cpp
@@ -1,8 +1,8 @@
 #include "GridPch.h"
 #include "Grid.h"
 
-size_t gMaxX = 0;
-size_t gMaxY = 0;
+//size_t gMaxX = 0;
+//size_t gMaxY = 0;
 
 Grid::Grid()
     : QGraphicsView()
@@ -19,19 +19,21 @@ Grid::Grid()
     _columnWidth = _fontMetrics.horizontalAdvance("000,000");
     _rowHeight = _fontMetrics.height();
 
+    _scene = new QGraphicsScene;
+
     // Draw column vertical lines
     for(size_t x = columnCount() * columnWidth(); x > 0; x -= columnWidth())
-      _scene.addLine(x, 0, x, rowCount() * rowHeight())->setPen(pen);
+      _scene->addLine(x, 0, x, rowCount() * rowHeight())->setPen(pen);
 
     // Draw row horizontal lines
     for(size_t y = rowCount() * rowHeight(); y > 0; y -= rowHeight())
-      _scene.addLine(0, y, columnCount() * columnWidth(), y)->setPen(pen);
+      _scene->addLine(0, y, columnCount() * columnWidth(), y)->setPen(pen);
 
     _rows.reserve(rowCount());
     for(size_t y = 0; y < rowCount(); y++)
         _rows.emplace_back(*this, y);
 
-    QGraphicsView::setScene(&_scene);
+    QGraphicsView::setScene(_scene);
 
     connect(verticalScrollBar(), &QScrollBar::valueChanged, this, &Grid::onVerticalScroll);
     connect(horizontalScrollBar(), &QScrollBar::valueChanged, this, &Grid::onHorizontalScroll);
@@ -40,7 +42,7 @@ Grid::Grid()
     connect(_timer, &QTimer::timeout, this, QOverload<>::of(&Grid::onTimer));
     _timer->start(1000);
 
-    qDebug("max x:%zu y:%zu", gMaxX, gMaxY);
+    //qDebug("max x:%zu y:%zu", gMaxX, gMaxY);
 }
 
 void Grid::onTimer()
--- Grid.h
+++ Grid.h
@@ -14,7 +14,7 @@ public:
     size_t rowCount() const { return 600; }
     size_t columnWidth() const { return _columnWidth; }
     size_t rowHeight() const { return _rowHeight; }
-    QGraphicsScene& scene() { return _scene; }
+    QGraphicsScene& scene() { return *_scene; }
     const QFontMetrics& fontMetrics() const { return _fontMetrics; }
 
 public slots:
@@ -29,13 +29,13 @@ protected:
     void calcRows();
     void calcColumns();
 
-    QGraphicsScene _scene;
-    QTimer*        _timer{};
-    Rows           _rows;
-    QFont          _font;
-    QFontMetrics   _fontMetrics;
-    QRect          _area;
-    Rows::iterator _itRowStart, _itRowEnd;
-    size_t         _columnStart{}, _columnEnd{1};
-    size_t         _rowHeight{}, _columnWidth{};
+    QGraphicsScene* _scene {};
+    QTimer*         _timer{};
+    Rows            _rows;
+    QFont           _font;
+    QFontMetrics    _fontMetrics;
+    QRect           _area;
+    Rows::iterator  _itRowStart, _itRowEnd;
+    size_t          _columnStart{}, _columnEnd{1};
+    size_t          _rowHeight{}, _columnWidth{};
 };
